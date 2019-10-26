9691ede1213252a2c3ddc2f2d6dc8fda46def0cd
Enhirosi <56927566+Enhirosi@users.noreply.github.com>
2019-10-26 16:36:49 +0900

    Human

--- src/jp/dcnet/yh/chapter07/Human.java
+++ src/jp/dcnet/yh/chapter07/Human.java
@@ -1,9 +1,51 @@
 package jp.dcnet.yh.chapter07;
 
 public class Human {
-	
-	public static void main(String[]args) {
-		
+	//名前
+				String humanProName ;
+	//年齢
+				int humanProAge;
+	//性別
+				boolean humanProSex ;
+
+				Human(){
+
+				}
+
+
+
+
+
+				public Human(String humanProName, int humanProAge, boolean humanProSex) {
+		super();
+		this.humanProName = humanProName;
+		this.humanProAge = humanProAge;
+		this.humanProSex = humanProSex;
 	}
+
+
+
+
+				public String getHumanProName() {
+					return humanProName;
+				}
+				public void setHumanProName(String humanProName) {
+					this.humanProName = humanProName;
+				}
+				public int getHumanProAge() {
+					return humanProAge;
+				}
+				public void setHumanProAge(int humanProAge) {
+					this.humanProAge = humanProAge;
+				}
+				public boolean isHumanProSex() {
+					return humanProSex;
+				}
+				public void setHumanProSex(boolean humanProSex) {
+					this.humanProSex = humanProSex;
+				}
+
+
+
 
 }
---
+++ src/jp/dcnet/yh/chapter07/Kicker.java
@@ -0,0 +1,34 @@
+package jp.dcnet.yh.chapter07;
+
+public class Kicker {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+//		Human human1 = new Human();
+//		human1.setHumanProName("大神");
+//		human1.setHumanProAge(32);
+//		human1.setHumanProSex(true);
+//		System.out.println(human1.humanProName);
+//		System.out.println(human1.humanProAge);
+//		System.out.println(human1.humanProSex);
+
+
+
+
+		Human human2 = new Human("李",31,true);
+		System.out.println(human2.humanProName);
+		System.out.println(human2.humanProAge);
+//		System.out.println(human2.humanProSex);
+		if(human2.isHumanProSex()) {
+			System.out.println("男");
+		}else{
+				System.out.println("女");
+			}
+
+
+
+
+	}
+
+}
