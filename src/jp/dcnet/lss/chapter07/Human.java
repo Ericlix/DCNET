65a665f2f8fb17370a6bf1688c7fddb930f5787b
lss <linke@lin>
2019-10-26 16:34:21 +0900

    新規 20191026 lss パッケージ新規作成

--- src/jp/dcnet/lss/chapter07/Human.java
+++ src/jp/dcnet/lss/chapter07/Human.java
@@ -2,9 +2,65 @@ package jp.dcnet.lss.chapter07;
 
 public class Human {
 
-	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
+	//名前
+	String humanProName;
+
+	//年齢
+	int humanProAge;
+
+	//性別
+	boolean humanProSex;
+
+	//コンストラクタ:1.メソッド　＝クラス名2戻り値タイプの指定不要
+
+	Human(String humanProName, int humanProAge, boolean humanProSex) {
+		this.humanProName = humanProName;
+		this.humanProAge = humanProAge;
+		this.humanProSex = humanProSex;
+	}
+
+	public String getHumanProName() {
+		return humanProName;
+	}
+
+
+	public void setHumanProName(String humanProName) {
+		this.humanProName = humanProName;
+	}
+
+	public int getHumanProAge() {
+		return humanProAge;
+	}
+
+	public void setHumanProAge(int humanProAge) {
+		this.humanProAge = humanProAge;
+	}
+
+	public boolean isHumanProSex() {
+		return humanProSex;
+	}
+
+	public void setHumanProSex(boolean humanProSex) {
+		this.humanProSex = humanProSex;
+	}
+
+	//普通メソッド　戻り値タイプの指定　　引数の指定
+//	void addTex(int i){
+//
+//	}
+	//コンストラクタ
+	Human(){
 
 	}
+
+
+
+//	public static void main(String[] args) {
+//
+//
+//
+//
+//
+//	}
 
 }
---
+++ src/jp/dcnet/lss/chapter07/kicker.java
@@ -0,0 +1,32 @@
+package jp.dcnet.lss.chapter07;
+
+public class kicker {
+
+	public static void main(String[] args) {
+		//実行プロック
+		
+		
+
+		//タイプ　obj名称　＝new　コンストラクタ(引数)
+		Human human1 = new Human();
+		human1.setHumanProName("a");
+		human1.setHumanProAge(31);
+		human1.setHumanProSex(true);
+		System.out.print(human1.humanProName);
+		System.out.print(human1.humanProAge);
+		System.out.println(human1.humanProSex);
+
+
+		Human human2 = new Human("b",31,true);
+		System.out.print(human2.getHumanProName());
+		System.out.print(human2.getHumanProAge());
+		if(human2.isHumanProSex()){
+			System.out.println("男性");
+		}else{
+		System.out.println("女性");
+		}
+
+
+	}
+
+}
