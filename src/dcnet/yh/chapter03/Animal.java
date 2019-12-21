764d8cc7d6bd2e1297b31e84f941726317df0510
Enhirosi <56927566+Enhirosi@users.noreply.github.com>
2019-12-21 15:37:26 +0900

    OOP Quesstion

---
+++ src/dcnet/yh/chapter03/Animal.java
@@ -0,0 +1,29 @@
+package dcnet.yh.chapter03;
+
+public class Animal {
+	private int age;
+	private String name;
+	private String kind;
+
+	public Animal(int age, String name, String kind) {
+		this.age = age;
+		this.kind = kind;
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+
+	public String getKind() {
+		return kind;
+	}
+
+
+}
---
+++ src/dcnet/yh/chapter03/K.java
@@ -0,0 +1,22 @@
+package dcnet.yh.chapter03;
+
+public class K  {
+	public static void main (String[]args) {
+
+		Cat cat = new Cat(5,"Lucky","xx",4);
+	cat.getAge();
+	cat.getKind();
+	cat.getName();
+	cat.getLegs();
+	System.out.println(cat.getAge()+cat.getKind()+cat.getName()+cat.getLegs());
+
+
+
+
+	}
+
+
+}
+
+
+
