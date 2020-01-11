a400a0c77856130b451d10a8e1d2ac18d750eca8
linke <linke@192.168.1.31>
2020-01-11 18:45:47 +0900

    20200111 チャジ

--- src/jp/dcnet/lss/chapter01/Q1余数.java
+++ src/jp/dcnet/lss/chapter01/Q1余数.java
@@ -5,7 +5,17 @@ public class Q1余数 {
 		   int i = 10;
 		   int n = i%5;
 		   System.out.println(n);
-	 }
+
+		   String s1 = new String("ABC");
+		   String s2 = new String("ABC");
+		   if(s1 == s2){
+			   System.out.println("s1=s2");
+		   }else{
+			   System.out.println("s1 !=s2");
+		   }
+		   
+	}
+	 
 
 
 
