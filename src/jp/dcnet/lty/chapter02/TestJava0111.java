dc5b765606879e95ff2a81f589d6bd2f36fbcf0b
txq6163 <txq1559240028@yahoo.co.jp>
2020-01-13 15:49:32 +0900

    lty 20200113 02

---
+++ src/jp/dcnet/lty/chapter02/TestJava0111.java
@@ -0,0 +1,18 @@
+package jp.dcnet.lty.chapter02;
+
+public class TestJava0111 {
+	public static void main(String[] args) {
+		System.out.println("this is line1\tthis line2");
+		String a = "x";
+		String b = "x";
+		System.out.println(a == b);
+		System.out.println(a.equals(b));
+		System.out.println(String.valueOf("1.0F"));
+		System.out.println(1.0F + "");
+		System.out.println(1.0 + 2.0);
+		System.out.println(1.0 + "2.0");
+		int c = 10;
+		System.out.println("c");
+	}
+
+}
